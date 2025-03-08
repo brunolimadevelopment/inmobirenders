c7863db64b2ed8657127d7f745c7cf125ce7055c
jose.gomes <jose.gomes@petrosoftdesign.com>
2025-03-08 01:14:18 -0300

    componentizando cardCheckout

--- src/components/cardCheckout.jsx
+++ src/components/cardCheckout.jsx
@@ -1,25 +1,34 @@
 import Button from "../components/button";
-
 import CardCheckoutListItem from "./cardCheckoutListItem"
-export default function cardCheckout() {
+
+export default function cardCheckout({ tarja, width, text, description, price, discount, plus }) {
+
   return (
-    <div className="pt-[64px] pb-[64px] pl-[16px] pr-[16px] bg-[white] h-[905px] w-[469px] flex flex-col items-center justify-between">
-
-      <div className="h-[136px] w-full flex flex-col items-center text-center text-white bg-[#f1561f] pt-[20px]">
-        <h2 className="font-reem-kufi text-[1.4rem] font-medium uppercase leading-[36px] tracking-[1px]">1 Render Estático <br />en pack x 5u</h2>
-        <span className="font-lato text-[0.9rem] font-bold uppercase leading-[1.4em] tracking-[1.8px]">imagen plana</span>
-      </div>
-
-      <div class="flex flex-wrap justify-center items-end font-josefin text-[60px] font-black text-[#555] py-10">
-        <div class="flex items-end me-4 line-through text-base font-normal">
-          <span class="text-black leading-none me-1">€</span><span className="leading-none">80</span>
+    <div className=" pt-[64px] pb-[64px] pl-[16px] pr-[16px] bg-[white] h-[905px] w-[469px] flex flex-col items-center justify-between">
+      <div className="relative h-[136px] w-full flex flex-col gap-[10px] items-center text-center text-white bg-[#f1561f] pt-[20px]">
+        <h2 className="font-reem-kufi text-[1.4rem] font-medium uppercase leading-[33px] tracking-[1px]" style={{ width: width}}>{text}</h2>
+        <span className="font-lato text-[0.9rem] font-bold uppercase leading-[1.4em] tracking-[1.8px]">{description}</span>
+        {tarja && (
+        <div class="absolute top-0 right-0 w-[150px] h-[150px] overflow-hidden rotate-90">
+          <div class="bg-black text-white text-[13px] font-extrabold text-center uppercase leading-[2] mt-[35px] w-[200%] 
+            transform -translate-y-[13.33px] -translate-x-[113px] -rotate-45">
+            Popular
+          </div>
         </div>
-        <div className="flex">
-          <span class="text-black text-[0.3em] self-start leading-none me-1">€</span>
-          <span class="leading-[40px] text-black">50</span>
-        </div>
+        )}
       </div>
-
+      <div class="flex flex-col font-josefin text-[60px] font-black text-[#555] py-10">
+        <div class="flex flex-wrap justify-center items-end">
+          <div class="flex items-end me-4 line-through text-base font-normal">
+            <span class="text-black leading-none me-1">€</span><span className="leading-none">{price}</span>
+          </div>
+          <div className="flex items-center">
+            <span class="text-black text-[0.3em] self-start leading-none me-1">€</span>
+            <span class="leading-[40px] text-black">{discount}</span>
+          </div>
+        </div>
+        <p className="mt-[15px] text-black text-center font-roboto-mono text-[14px] font-light leading-[1em]">{plus}</p>
+      </div>
       <div className="flex flex-col pl-[20px] pr-[20px] ">
         <CardCheckoutListItem text="Trabajo desde plano o boceto" />
         <CardCheckoutListItem text="2 Rondas de modificaciones" />
@@ -28,10 +37,7 @@ export default function cardCheckout() {
         <CardCheckoutListItem text="Render Exterior +30%" />
         <CardCheckoutListItem text="Renders Comerciales, industriales o especiales consultar" />
       </div>
-
       <Button className="w-[252px] h-[73px] bg-[#F1561F] hover:bg-white" text="¡quiero mi render!" url="https://api.whatsapp.com/send?phone=34634050913"/>
-    
     </div>
-
   )
 }
--- src/components/checkout.jsx
+++ src/components/checkout.jsx
@@ -6,8 +6,8 @@ export default function checkout() {
         <div className="container">
             <h2 className="text-center text-[35px] font-bold text-white uppercase font-[Reem Kufi] leading-tight tracking-[2px] leading-[1.2] pt-[80px] pb-[80px]">¡Promoción otoño/invierno!</h2>
             <div className="flex justify-center gap-[20px]">
-              <CardCheckout />
-              <CardCheckout />
+              <CardCheckout tarja={false} width={270} text={'1 Render Estático en pack x 5u'} description={'imagen plana'} price={80} discount={50} plus={null} />
+              <CardCheckout tarja={true} width={140} text={'Visual 360º'} description={'por estancia hasta 50 m2'} price={190} discount={95} plus={'+ IVA'} />
             </div>
         </div>
     </section>

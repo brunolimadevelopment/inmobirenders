5d0f1f642e23323dde8c368c7a060efb66a1eb4e
jose.gomes <jose.gomes@petrosoftdesign.com>
2025-03-08 00:07:19 -0300

    load section checkout

--- src/components/checkout.jsx
+++ src/components/checkout.jsx
@@ -5,7 +5,10 @@ export default function checkout() {
     <section className="w-full pt-[40px] pb-[40px]" style={{ backgroundImage: "url('src/images/render-scaled.jpg')" }}>
         <div className="container">
             <h2 className="text-center text-[35px] font-bold text-white uppercase font-[Reem Kufi] leading-tight tracking-[2px] leading-[1.2] pt-[80px] pb-[80px]">¡Promoción otoño/invierno!</h2>
-            <CardCheckout />
+            <div className="flex justify-center gap-[20px]">
+              <CardCheckout />
+              <CardCheckout />
+            </div>
         </div>
     </section>
   )
